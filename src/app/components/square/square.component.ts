16541af40a001fdd45c17edf8b41bdccb6ae1637
davideTwin <oreglio@twin-online.it>
2023-03-31 14:43:55 +0200

    refac metodi board

--- src/app/components/square/square.component.ts
+++ src/app/components/square/square.component.ts
@@ -2,9 +2,13 @@ import { Component, Input } from '@angular/core';
 
 @Component({
    selector: 'app-square',
-   template: ` <button>{{ value }}</button> `,
+   template: `
+      <button>
+         {{ value }}
+      </button>
+   `,
    styleUrls: ['./square.component.scss'],
 })
 export class SquareComponent {
-   @Input() value!: 'X' | 'O';
+   @Input() value!: string;
 }
---
+++ src/app/xo.ts
@@ -0,0 +1,3 @@
+export interface Xo {
+   value: 'X' | 'O';
+}

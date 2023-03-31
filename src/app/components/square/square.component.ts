9416cb1128ac28e8c9c08c0a0ef86f4a841244ba
davideTwin <oreglio@twin-online.it>
2023-03-31 12:53:07 +0200

    create components folder & board file

---
+++ src/app/components/square/square.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+   selector: 'app-square',
+   template: ` <button>{{ value }}</button> `,
+   styleUrls: ['./square.component.scss'],
+})
+export class SquareComponent {
+   @Input() value!: 'X' | 'O';
+}
